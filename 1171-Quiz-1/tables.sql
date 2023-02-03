b7d22a608ac7875f81a00d4d7495a9845d2188fb
AKEYLAHHERNANDEZ <119026429+AKEYLAHHERNANDEZ@users.noreply.github.com>
2023-02-03 13:44:30 -0600

    Add files via upload

---
+++ 1171-Quiz-1/tables.sql
@@ -0,0 +1,19 @@
+-- This is my 'tables.sql' file 
+DROP TABLE IF EXISTS employees;
+
+
+CREATE TABLE employees( 
+employee_id serial PRIMARY KEY,
+name text NOT NULL,
+job text NOT NULL,
+salary int NOT NULL
+);
+
+INSERT INTO employees (name,job,salary) 
+values 
+  ('Daniel', 'Nurse', '3600'),
+  ('Corey', 'Doctor', '5000'), 
+  ('Violet', 'Software Engineer','3200'),
+  ('Akeylah', 'Programmer', '50000'),
+  ('Gabriela', 'Artist', '1500');
+
